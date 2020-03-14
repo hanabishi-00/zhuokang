aca3e459c148f90ccf8f31f2006ec48c38669d2d
shisanshe <411877021@qq.com>
2020-03-14 22:26:40 +0800

    CharUtil中添加UUID

--- src/main/java/com/huake/device/util/CharUtil.java
+++ src/main/java/com/huake/device/util/CharUtil.java
@@ -1,29 +1,10 @@
 package com.huake.device.util;
 
-import java.util.Random;
+import java.util.UUID;
 
 public class CharUtil {
-
-    public static String getRandomString(Integer num) {
-        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
-        Random random = new Random();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < num; i++) {
-            int number = random.nextInt(base.length());
-            sb.append(base.charAt(number));
-        }
-        return sb.toString();
+    // 获取UUID
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
     }
-
-    public static String getRandomNum(Integer num) {
-        String base = "0123456789";
-        Random random = new Random();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < num; i++) {
-            int number = random.nextInt(base.length());
-            sb.append(base.charAt(number));
-        }
-        return sb.toString();
-    }
-
 }
